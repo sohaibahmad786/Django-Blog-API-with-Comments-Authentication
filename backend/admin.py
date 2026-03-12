4a25b27af9e994e332e9cea03603571af8cc0fc2
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-03-12 23:13:39 +0500

    Update admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,19 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Student
 from .models import Register
-from .models import Note
-from .models import Category
-from .models import Products
 from .models import Post
 from .models import Comment
 
 admin.site.register(Comment)
 admin.site.register(Post)
-admin.site.register(Products)
-admin.site.register(Category)
-admin.site.register(Note)
 admin.site.register(Register,UserAdmin)
-admin.site.register(Student)
 # Register your models here.
