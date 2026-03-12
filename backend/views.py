2df6c26fc7ba933d66b484e585952b63f7cff5e6
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-03-12 23:19:52 +0500

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -12,44 +12,14 @@ from rest_framework import generics,filters
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Student
-from .serializer import Student_serilizer
 from django.conf import settings
 from rest_framework.filters import SearchFilter
 from datetime import datetime, timedelta
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Register
 from .serializer import Register_serilizer
-from .models import Note
-from .serializer import Note_serilizer
-from .models import Category,Products
-from .serializer import category_serilizer,products_serilizer
 from .models import Post,Comment
 from .serializer import Comment_serilizer,Post_serilizer
-
-
-  
-# ______________________ Student Managment API___________________
-class student_list(generics.ListCreateAPIView):
-    queryset=Student.objects.all()
-    serializer_class=Student_serilizer
-class student_detail(generics.RetrieveUpdateDestroyAPIView):
-    queryset=Student.objects.all()
-    serializer_class=Student_serilizer
-    
-
-# _____________________ Banking Account API __________
-# class account_list(generics.ListCreateAPIView):
-#     queryset=Account.objects.all()
-#     serializer_class=Account_serilizer
-#     authentication_classes=[JWTAuthentication]
-#     permission_classes=[permissions.AllowAny]
-
-# class account_detail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset=Account.objects.all()
-#     serializer_class=Account_serilizer
-#     authentication_classes=[JWTAuthentication]
-#     permission_classes=[permissions.AllowAny]
 
 
 # ___________________ register________
@@ -58,41 +28,6 @@ class Register_list(ListCreateAPIView):
     queryset=Register.objects.all()
     serializer_class=Register_serilizer
     permission_classes=[permissions.AllowAny]
-
-
-class Note_list(generics.ListCreateAPIView):
-
-    queryset=Note.objects.all()
-    serializer_class=Note_serilizer
-    authentication_classes=[JWTAuthentication,SessionAuthentication]
-    permission_classes=[permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-   
-class Note_detail(generics.RetrieveUpdateDestroyAPIView):
-
-    queryset=Note.objects.all()
-    serializer_class=Note_serilizer
-    authentication_classes=[JWTAuthentication,SessionAuthentication]
-    permission_classes=[permissions.IsAuthenticated]
-
-
-# _______________ Products API view _________________
-class Productlistview(generics.ListCreateAPIView):
-    queryset=Products.objects.all()
-    serializer_class=products_serilizer
-    authentication_classes=[JWTAuthentication,SessionAuthentication]
-    permission_classes=[permissions.IsAuthenticated]
-
-    filter_backends=[DjangoFilterBackend,filters.SearchFilter]
-    filterset_fields=['category']
-    search_fields=['name']
-class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset=Products.objects.all()
-    serializer_class=products_serilizer
-    authentication_classes=[JWTAuthentication,SessionAuthentication]
-    permission_classes=[permissions.IsAuthenticated]
 
 # _______________________ Blog APi_______________________
 class postlist(generics.ListCreateAPIView):
